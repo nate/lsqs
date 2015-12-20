cb4b118028a70ce3024bd9133a5bd9aad3a2229b
Giannis Melidis <giannismelidis@olery.com>
2015-12-20 11:49:04 +0100

    Created xml template class and added tests.

---
+++ lib/lsqs/xml_template.rb
@@ -0,0 +1,38 @@
+module LSQS
+  class XMLTemplate
+    attr_reader :template
+    
+    TEMPLATE_DIR = File.expand_path('../../../config', __FILE__)
+    
+    def initialize
+      xml = File.read("#{TEMPLATE_DIR}/template.xml.liquid")
+      @template = Liquid::Template.parse(xml)
+    end
+    
+    ##
+    # Renders the XML that is required as a body response.
+    #
+    # @param [Actions::Base] action
+    #
+    # @return [String]
+    #
+    def render(action)
+      options = { 
+        'action'     => action.name,
+        'content'    => action.to_xml,
+        'request_id' => request_id
+      }
+      
+      template.render(options)
+    end
+    
+    ##
+    # Generates a request ID.
+    #
+    # @return [String]
+    #
+    def request_id
+      SecureRandom.uuid
+    end
+  end # XMLTemplate
+end #LSQS
---
+++ spec/lsqs/xml_template_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe LSQS::XMLTemplate do
+  before do
+    @xml_template = described_class.new
+  end
+  
+  describe '#initialize' do
+    it 'initializes with a liquid template' do
+      @xml_template.template.kind_of?(Liquid::Template).should be_truthy
+    end
+  end
+  
+  describe '#render' do
+    before do
+      @action = OpenStruct.new(:name => 'Foo', :to_xml => '<Bar>Foo Bar</Bar>')
+      @result = @xml_template.render(@action)
+    end
+    it 'renders XML and returns a string' do      
+      @result.kind_of?(String).should be_truthy
+    end
+    
+    it 'includes opening closing xml tags' do
+      @result.include?('<xml').should be_truthy
+      @result.include?('</xml>').should be_truthy
+    end
+    
+    it 'includes the result name properly' do
+      @result.include?("<#{@action.name}Result>").should be_truthy
+      @result.include?("</#{@action.name}Result>").should be_truthy
+    end
+    
+    it 'includes the content' do
+      @result.include?(@action.to_xml).should be_truthy
+    end
+  end
+  
+  describe '#request_id' do
+    it 'returns a string' do
+      @xml_template.request_id.kind_of?(String).should be_truthy
+    end
+  end
+end
