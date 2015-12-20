dd935afa9c98a9238a671ba6acffe15bbd5c7158
Giannis Melidis <giannismelidis@olery.com>
2015-12-20 11:48:04 +0100

    Created server class.

---
+++ lib/lsqs/server.rb
@@ -0,0 +1,51 @@
+module LSQS
+  class Server < Sinatra::Base
+    helpers do
+      
+      ##
+      # Gets the action that was requested.
+      #
+      # @return [String]
+      #
+      def action
+        params['Action']
+      end
+      
+      ##
+      # Retrieves a XML template and renders it.
+      #
+      # @param [LSQS::Actions::Base] action
+      #
+      # @return [String]
+      # 
+      def render(action)
+        LSQS.template.render(action)
+      end
+      
+      ##
+      # Returns the base URL of the server.
+      #
+      # @return [String]
+      #
+      def base_url
+        request.base_url
+      end
+    end
+    
+    post '/' do
+      params['base_url'] = base_url
+      
+      require 'pry'; binding.pry
+      
+      result = LSQS.router.distribute(action, params)
+  
+      return {:body => render(result)}.to_json
+    end
+    
+    post "/:queue" do
+      result = LSQS.router.distributel(action, params)
+      
+      return {:body => render(result)}.to_json
+    end 
+  end # Server
+end # LSQS
