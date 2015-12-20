fda8c7dceb08a0aa751125377bb6caf0c9d4d191
Giannis Melidis <giannismelidis@olery.com>
2015-12-20 17:29:24 +0100

    Added few requirements in main file.

--- lib/lsqs.rb
+++ lib/lsqs.rb
@@ -2,26 +2,34 @@ require 'sinatra'
 require 'puma/cli'
 require 'liquid'
 require 'securerandom'
+require 'digest/md5'
 require 'active_support/core_ext/string'
 require 'json'
 require 'builder'
 
 require_relative 'lsqs/version'
+require_relative 'lsqs/error_handler'
 require_relative 'lsqs/server'
 require_relative 'lsqs/xml_template'
 require_relative 'lsqs/action_router'
 require_relative 'lsqs/queue_list'
+require_relative 'lsqs/queue'
+require_relative 'lsqs/message'
 
 require_relative 'lsqs/actions/base'
 require_relative 'lsqs/actions/get_queue_url'
+require_relative 'lsqs/actions/receive_message'
+require_relative 'lsqs/actions/create_queue'
+require_relative 'lsqs/actions/send_message'
+require_relative 'lsqs/actions/delete_message_batch'
 
 module LSQS
   def self.template
-    XMLTemplate.new
+    @template ||= XMLTemplate.new
   end
   
   def self.router
-    ActionRouter.new(queue_list)
+    @router ||= ActionRouter.new(queue_list)
   end
   
   def self.queue_list
