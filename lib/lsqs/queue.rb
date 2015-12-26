a0515d7529619b774efb4311f15e27030eb3c313
Giannis Melidis <giannismelidis@olery.com>
2015-12-26 16:49:41 +0200

    Created queue class and tests.

---
+++ lib/lsqs/queue.rb
@@ -0,0 +1,173 @@
+module LSQS
+  class Queue
+    attr_accessor :name, :messages, :in_flight, :attributes, :monitor
+    
+    DEFAULT_TIMEOUT = 30
+    
+    def initialize(name, params = {})
+      @name       = name
+      @attributes = params.fetch('Attributes'){Hash.new}
+      @monitor    = Monitor.new
+      @messages   = []
+      @in_flight  = {}
+      
+      check_timeout
+    end
+    
+    ##
+    # Sets the default timeout of a queue. It takes the value from the
+    # attributes, if it is set, otherwise it uses the `DEFAULT_TIMEOUT`
+    # constant.
+    #
+    # @return [Fixnum]
+    #
+    def visibility_timeout
+      attributes['VisibilityTimeout'] || DEFAULT_TIMEOUT
+    end
+    
+    ##
+    # Creates a new message in the queue.
+    #
+    # @param [Hash] options
+    #
+    # @return [Message]
+    #
+    def create_message(options = {})
+      lock do
+        message = Message.new(options)
+        @messages << message
+        return message
+      end
+    end
+    
+    ##
+    # Gets a number of messages based on the MaxNumberOfMessages field.
+    #
+    # @param [Hash] options
+    #
+    # @return [Hash]
+    #
+    def get_messages(options = {})
+      number_of_messages = options.fetch('MaxNumberOfMessages'){1}
+
+      raise 'ReadCountOutOfRange' if number_of_messages > 10
+
+      result = {}
+
+      lock do
+        amount = number_of_messages > size ? size : number_of_messages
+
+        amount.times do
+          message             = messages.delete_at(rand(size))
+          message.expire_in(visibility_timeout)
+          receipt             = generate_receipt
+          @in_flight[receipt] = message
+          result[receipt]     = message
+        end
+      end
+
+      return result
+    end
+    
+    ##
+    # Deletes a message from the messages that are in-flight.
+    #
+    # @param [String] receipt
+    #
+    def delete_message(receipt)
+      lock do
+        in_flight.delete(receipt)
+      end
+    end
+    
+    ##
+    # Deletes all messages in queue and in flight.
+    #
+    def purge
+      lock do
+        @messages  = []
+        @in_flight = {}
+      end
+    end
+    
+    ##
+    # Returns the amount of messages in the queue.
+    #
+    # @return [Fixnum]
+    #
+    def size
+      messages.size
+    end
+    
+    ##
+    # Generates a hex receipt for the message
+    #
+    # @return [String]
+    #
+    def generate_receipt
+      SecureRandom.hex(16)
+    end
+    
+    ##
+    # Change the visibility of a message that is in flight. If visibility
+    # is set to 0, put back in the queue.
+    #
+    # @param [String] receipt
+    # @param [Fixnum] seconds
+    #
+    def change_message_visibility(receipt, seconds)
+      lock do
+        message = @in_flight[receipt]
+        raise 'MessageNotInflight' unless message
+
+        if seconds == 0
+          message.expire
+          @messages << message
+          delete_message(receipt)
+        else
+          message.expire_in(seconds)
+        end
+      end
+    end
+    
+    ##
+    # Initializes a thread that checks every 5 seconds if messages need
+    # to be put back from flight to the message queue.
+    #
+    def check_timeout
+      Thread.new do
+        while true
+          unless in_flight.empty?
+            timeout_messages
+          end
+          sleep(5)
+        end
+      end
+    end
+    
+    ##
+    # Checks if in-fligh messages need to be put back in the queue.
+    # 
+    def timeout_messages
+      lock do
+        in_flight.each do |key, message|
+          if message.expired?
+            message.expire
+            @messages << message
+            delete_message(key)
+          end
+        end
+      end
+    end
+    
+    ##
+    # Locks a block, in order to ensure that there is no conflict if more than
+    # one processes try to access the object.
+    # 
+    def lock
+      @monitor.synchronize do
+        yield
+      end
+    end
+  end # Queue
+end # LSQS
---
+++ spec/lsqs/queue_spec.rb
@@ -0,0 +1,193 @@
+require 'spec_helper'
+
+describe LSQS::Queue do
+  before do
+    @queue = described_class.new('test')
+    @queue.purge
+  end
+  
+  describe '#initialize' do
+    it 'has a name' do      
+      @queue.name.should == 'test'
+    end
+    
+    it 'has a messages array' do
+      @queue.messages.kind_of?(Array).should be_truthy
+      @queue.messages.empty?.should be_truthy
+    end
+    
+    it 'has an in_flight hash' do
+      @queue.in_flight.kind_of?(Hash).should be_truthy
+      @queue.in_flight.empty?.should be_truthy
+    end
+    
+    it 'has attributes hash' do
+      @queue.attributes.kind_of?(Hash).should be_truthy
+    end
+  end
+  
+  describe '#default_timeout' do
+    it 'has default visibility if not set' do
+      queue = described_class.new('test')
+      
+      queue.visibility_timeout.should == described_class::DEFAULT_TIMEOUT
+    end
+    
+    it 'has visibility timeout if set' do
+      queue = described_class.new('test', {'Attributes' => {'VisibilityTimeout' => 10}})
+      
+      queue.visibility_timeout.should == 10
+    end
+  end
+  
+  describe '#create_message' do
+    it 'creates a message' do
+      @queue.create_message
+      
+      @queue.messages.size.should == 1
+      @queue.messages.first.kind_of?(Message).should be_truthy
+    end
+  end
+  
+  describe '#get_messages' do
+    it 'gets a single message' do
+      @queue.create_message('MessageBody' => 'foo')
+      
+      result = @queue.get_messages
+      
+      result.size.should == 1
+      @queue.in_flight.size.should == 1
+      result.find{|k,v| v.body == 'foo'}.should be_truthy
+    end
+    
+    it 'gets a two messages if MaxNumberOfMessages is 2' do
+      @queue.create_message('MessageBody' => 'foo')
+      @queue.create_message('MessageBody' => 'bar')
+      @queue.create_message('MessageBody' => 'foo bar')
+      
+      result = @queue.get_messages('MaxNumberOfMessages' => 2)
+      
+      result.size.should == 2
+      @queue.in_flight.size.should == 2
+    end
+    
+    it 'gets a two messages if MaxNumberOfMessages is 5 and available are 2' do
+      @queue.create_message('MessageBody' => 'foo')
+      @queue.create_message('MessageBody' => 'bar')
+      
+      result = @queue.get_messages('MaxNumberOfMessages' => 5)
+      
+      result.size.should == 2
+      @queue.in_flight.size.should == 2
+    end
+    
+    it 'throws an error if max number is above 10' do
+      @queue.create_message('MessageBody' => 'foo')
+      @queue.create_message('MessageBody' => 'bar')
+      
+      expect{@queue.get_messages('MaxNumberOfMessages' => 11)}.to raise_error(RuntimeError, 'ReadCountOutOfRange')
+    end
+  end
+  
+  describe '#delete_message' do
+  end
+  
+  describe '#size' do
+    it 'calculates size correctly' do
+      @queue.create_message('MessageBody' => 'foo')
+      @queue.create_message('MessageBody' => 'bar')
+      @queue.create_message('MessageBody' => 'test')
+      
+      @queue.size.should == 3
+    end
+  end
+  
+  describe '#purge_queue' do
+    it 'clears the queue' do
+      @queue.create_message('MessageBody' => 'foo')
+      @queue.create_message('MessageBody' => 'bar')
+      
+      @queue.size.should == 2
+      @queue.in_flight.size.should == 0
+      
+      @queue.get_messages
+      
+      @queue.size.should == 1
+      @queue.in_flight.size.should == 1
+      
+      @queue.purge
+      
+      @queue.size.zero?.should be_truthy
+      @queue.in_flight.size.zero?.should be_truthy
+      
+    end
+  end
+  
+  describe '#generate_receipt' do
+    it 'generates a string' do
+      receipt = @queue.generate_receipt
+      receipt.kind_of?(String).should be_truthy
+      receipt.length.should == 32
+    end
+  end
+  
+  describe '#change_message_visibility' do
+    it 'puts the message back in the queue if seconds is 0' do
+      @queue.create_message
+      
+      @queue.size.should == 1
+      
+      result = @queue.get_messages
+      
+      @queue.size.zero?.should be_truthy
+      @queue.in_flight.size.should == 1
+      
+      @queue.change_message_visibility(result.first.first, 0)
+      
+      @queue.size.should == 1
+      @queue.in_flight.size.zero?.should be_truthy
+    end
+    
+    it 'throws an error if the message is not in flight' do
+      expect{@queue.change_message_visibility('foo', 0)}.to raise_error(RuntimeError, 'MessageNotInflight')
+    end
+    
+    it 'keeps the message in flight if seconds is more than 0' do
+      @queue.create_message
+      
+      @queue.size.should == 1
+      
+      result = @queue.get_messages
+      
+      @queue.size.zero?.should be_truthy
+      @queue.in_flight.size.should == 1
+      
+      @queue.change_message_visibility(result.first.first, 10)
+      
+      @queue.size.zero?.should be_truthy
+      @queue.in_flight.size.should == 1
+    end
+  end
+  
+  describe '#timeout_messages' do
+    it 'puts messages back in the queue' do
+      queue = described_class.new('test', {'Attributes' => {'VisibilityTimeout' => 1}})
+      
+      queue.create_message
+      
+      queue.size.should == 1
+      queue.in_flight.size.zero?.should be_truthy
+      
+      queue.get_messages
+      queue.size.zero?.should be_truthy
+      queue.in_flight.size.should == 1
+      
+      sleep(2)
+      
+      queue.timeout_messages
+      
+      queue.size.should == 1
+      queue.in_flight.size.zero?.should be_truthy
+    end
+  end
+end
